b38abf3867b295f58656fb5ae54505b1f6537cf6
unknown <arestov981405@gmail.com>
2019-04-19 06:23:28 +0300

    Added empty canvas.

--- scr.js
+++ scr.js
@@ -1,1 +1,12 @@
+let main = async function() 
+{
+	let w = 700; 
+	let h = 700;
+    $('body').append('<div></div>');
+    let pickBox = $('<canvas></canvas>');
+    pickBox.attr("height", h);
+    pickBox.attr("width", w);
+    pickBox.appendTo('div');
+};
 
+$( document ).ready(main());
